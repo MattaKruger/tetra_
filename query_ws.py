868cbc71b3976038036bdc5b94ce1f35ad42012e
matthijs <krugermatthijs@gmail.com>
2023-10-24 14:52:28 +0200

    cleanup

--- query_ws.py
+++
@@ -1,8 +0,0 @@
-from websockets.sync.client import connect
-
-with connect("ws://localhost:8000") as websocket:
-    websocket.send("Eureka!")
-    assert websocket.recv() == "Eureka!"
-
-    websocket.send("I've found it!")
-    assert websocket.recv() == "I've found it!"
